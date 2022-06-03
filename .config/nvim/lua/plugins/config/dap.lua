aa62906acc0da8f0a7ed5e18bb5d83365202d923
taraslis453 <taraslisiy54@gmail.com>
2022-06-04 00:14:56 +0300

    feat: setup debugger for go

--- .config/nvim/lua/plugins/config/dap.lua
+++ .config/nvim/lua/plugins/config/dap.lua
@@ -1,21 +1,47 @@
-local dap = require("dap")
+-- local dap = require("dap")
 
-dap.adapters.go = {
-	type = "executable",
-	command = "node",
-	args = { os.getenv("HOME") .. "/.vscode/extensions/golang.go-0.32.0/dist/debugAdapter.js" },
-}
+-- dap.adapters.go = {
+-- 	type = "executable",
+-- 	command = "node",
+-- 	args = { os.getenv("HOME") .. "/.vscode/extensions/golang.go-0.32.0/dist/debugAdapter.js" },
+-- }
+--
+-- dap.configurations.go = {
+-- 	{
+-- 		type = "go",
+-- 		name = "Debug server",
+-- 		request = "launch",
+-- 		showLog = false,
+-- 		program = "${workspaceFolder}/api-poc/cmd/main.go",
+-- 		dlvToolPath = vim.fn.exepath("dlv"),
+-- 	},
+-- }
+--
 
-dap.configurations.go = {
-	{
-		type = "go",
-		name = "Debug server",
-		request = "launch",
-		showLog = false,
-		program = "${workspaceFolder}/api-poc/cmd/main.go",
-		dlvToolPath = vim.fn.exepath("dlv"),
-	},
-}
+local function map(mode, lhs, rhs, opts)
+	local options = { noremap = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+map("n", "<space>db", ':lua require"dap".toggle_breakpoint()<CR>')
+map("n", "<space>dso", ':lua require"dap".step_out()<CR>')
+map("n", "<space>dsi", ':lua require"dap".step_into()<CR>')
+map("n", "<space>dsv", ':lua require"dap".step_over()<CR>')
+map("n", "<space>dc", ':lua require"dap".continue()<CR>')
+map("n", "<space>dn", ':lua require"dap".run_to_cursor()<CR>')
+map("n", "<space>de", ':lua require"dap".close()<CR>')
+map("n", "<space>dk", ':lua require"dap".up()<CR>zz')
+map("n", "<space>dj", ':lua require"dap".down()<CR>zz')
+map("n", "<space>dt", ':lua require"dap".terminate()<CR>')
+map("n", "<space>dr", ':lua require"dap".repl.toggle({}, "vsplit")<CR><C-w>l')
+map("n", "<space>dR", ':lua require"dap".clear_breakpoints()<CR>')
+map("n", "<space>dh", ':lua require"dap.ui.widgets".hover()<CR>')
+map("n", "<space>ds", ':lua local widgets=require"dap.ui.widgets";widgets.centered_float(widgets.scopes)<CR>')
+map("n", "<space>dac", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+map("n", "<space>du", ':lua require"dapui".toggle()<CR>')
+
 vim.highlight.create("DapBreakpoint", { ctermbg = 0, guifg = "#993939" }, false)
 vim.highlight.create("DapBreakpointRejected", { ctermbg = 0, guifg = "#61afef" }, false)
 vim.highlight.create("DapStopped", { ctermbg = 0, guifg = "#98c379" }, false)
@@ -26,6 +52,10 @@ vim.fn.sign_define(
 	{ text = "🟦", texthl = "", linehl = "", numhl = "DapBreakpointRejected" }
 )
 vim.fn.sign_define("DapStopped", { text = "🟩", texthl = "", linehl = "", numhl = "DapStopped" })
+
+require("dap-go").setup()
+require("nvim-dap-virtual-text").setup()
+
 require("dapui").setup({
 	icons = { expanded = "▾", collapsed = "▸" },
 	mappings = {
@@ -68,28 +98,13 @@ require("dapui").setup({
 	windows = { indent = 1 },
 })
 
-require("nvim-dap-virtual-text").setup()
-
-local function map(mode, lhs, rhs, opts)
-	local options = { noremap = true }
-	if opts then
-		options = vim.tbl_extend("force", options, opts)
-	end
-	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.after.event_initialized["dapui_config"] = function()
+	dapui.open()
 end
-
-map("n", "<space>db", ':lua require"dap".toggle_breakpoint()<CR>')
-map("n", "<space>dso", ':lua require"dap".step_out()<CR>')
-map("n", "<space>dsi", ':lua require"dap".step_into()<CR>')
-map("n", "<space>dsv", ':lua require"dap".step_over()<CR>')
-map("n", "<space>dc", ':lua require"dap".continue()<CR>')
-map("n", "<space>dn", ':lua require"dap".run_to_cursor()<CR>')
-map("n", "<space>de", ':lua require"dap".close()<CR>')
-map("n", "<space>dk", ':lua require"dap".up()<CR>zz')
-map("n", "<space>dj", ':lua require"dap".down()<CR>zz')
-map("n", "<space>dt", ':lua require"dap".terminate()<CR>')
-map("n", "<space>dr", ':lua require"dap".repl.toggle({}, "vsplit")<CR><C-w>l')
-map("n", "<space>dR", ':lua require"dap".clear_breakpoints()<CR>')
-map("n", "<space>dh", ':lua require"dap.ui.widgets".hover()<CR>')
-map("n", "<space>ds", ':lua local widgets=require"dap.ui.widgets";widgets.centered_float(widgets.scopes)<CR>')
-map("n", "<space>du", ':lua require"dapui".toggle()<CR>')
+dap.listeners.before.event_terminated["dapui_config"] = function()
+	dapui.close()
+end
+dap.listeners.before.before_exited["dapui_config"] = function()
+	dapui.close()
+end
---
+++ .config/nvim/lua/plugins/config/rooter.lua
@@ -0,0 +1,3 @@
+vim.cmd([[
+  let g:rooter_patterns = ['makefile', 'go.mod']
+]])
