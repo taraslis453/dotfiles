cbb7c91b45faf1209d3007d627470ff0d22a7a5b
taraslis453 <taraslisiy54@gmail.com>
2022-10-25 19:42:07 +0300

    fix: add checks for plugin before require, clean up kitty config, pin noice commit

--- .config/nvim/lua/plugins/config/hop.lua
+++ .config/nvim/lua/plugins/config/hop.lua
@@ -1,3 +1,7 @@
-require("hop").setup()
+local status_ok, hop = pcall(require, "hop")
+if not status_ok then
+	return
+end
+hop.setup()
 vim.api.nvim_set_keymap("n", "<space>s", ":HopChar2MW<cr>", { silent = true })
 vim.api.nvim_set_keymap("v", "<space>s", "<cmd>HopChar2MW<CR>", { noremap = true, silent = true })
--- .config/nvim/lua/plugins/config/todo-comments.lua
+++ .config/nvim/lua/plugins/config/todo-comments.lua
@@ -1,4 +1,8 @@
-require("todo-comments").setup({
+local status_ok, todocomments = pcall(require, "todo-comments")
+if not status_ok then
+	return
+end
+todocomments.setup({
 	highlight = {
 		pattern = [[.*<(KEYWORDS)\s*]],
 	},
